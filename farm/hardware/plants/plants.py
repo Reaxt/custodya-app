c0be6fdde2fdce440cba7f73e28afc11774c6119
liamkopke <liamkopke@gmail.com>
2023-04-21 19:50:51 -0400

    finished plants

--- farm/hardware/plants/plants.py
+++ farm/hardware/plants/plants.py
@@ -6,4 +6,45 @@ from sensors.moisture import MoistureSensor
 from sensors.temperature import TemperatureSensor
 from sensors.water import WaterSensor
 
+from base.sensors import AReading
+from base.actuators import ACommand
+
 from time import sleep
+
+FAN_ON_COMMAND = ACommand("fan", {"value": "on"})
+FAN_OFF_COMMAND = ACommand("fan", {"value": "off"})
+LED_ON_COMMAND = ACommand("led", {"value": "on"})
+LED_OFF_COMMAND = ACommand("led", {"value": "off"})
+
+if __name__ == "__main__":
+    fan = FanController()
+    led = LedController()
+
+    humidity = HumiditySensor()
+    temperature = TemperatureSensor()
+    moisture = MoistureSensor()
+    water = WaterSensor()
+
+    while True:
+        # fan control
+        if fan._current_state == FAN_ON_COMMAND:
+            if fan.validate_command(FAN_OFF_COMMAND):
+                fan.control_actuator(FAN_OF_COMMAND.data)
+            elif fan.validate_command(FAN_ON_COMMAND):
+                fan.control_actuator(FAN_ON_COMMAND.data)
+
+        # led control
+        if led._current_state == LED_ON_COMMAND:
+            if led.validate_command(LED_OFF_COMMAND):
+                led.control_actuator(LED_OF_COMMAND.data)
+            elif led.validate_command(LED_ON_COMMAND):
+                led.control_actuator(LED_ON_COMMAND.data)
+
+        # print actuator readings
+        print(temperature.read_sensor())
+        print(humidity.read_sensor())
+        print(moisture.read_sensor())
+        print(water.read_sensor())
+
+        # sleep
+        sleep(1)
