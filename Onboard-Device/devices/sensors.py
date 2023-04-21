64c3c0120ec5775804def969d12e2a71321ab9f7
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-04-21 20:33:57 +0100

    Motion sensors

--- Onboard-Device/devices/sensors.py
+++ Onboard-Device/devices/sensors.py
@@ -12,6 +12,7 @@ class AReading:
         # Add new reading types here.
         TEMPERATURE = 'temperature'
         HUMIDITY = 'humidity'
+        MOTION = 'motion'
 
     class Unit(str, Enum):
         """Enum defining all possible units for sensor measuremens.
@@ -19,6 +20,7 @@ class AReading:
         # Add new reading units here.
         CELCIUS = 'C'
         HUMIDITY = '% HR'
+        BOOLEAN = 'bool'
 
     def __init__(self, type: Type, unit: Unit, value: Any) -> None:
         """Create new AReading based on a type of reading its units and value
