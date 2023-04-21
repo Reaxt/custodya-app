1d7bf6143b99e5261e7222efd909ac84420af85a
liamkopke <liamkopke@gmail.com>
2023-04-21 19:31:19 -0400

    done actuators and sensors

---
+++ farm/hardware/plants/plants.py
@@ -0,0 +1,9 @@
+from actuators.fan import FanController
+from actuators.led import LedController
+
+from sensors.humidity import HumiditySensor
+from sensors.moisture import MoistureSensor
+from sensors.temperature import TemperatureSensor
+from sensors.water import WaterSensor
+
+from time import sleep
