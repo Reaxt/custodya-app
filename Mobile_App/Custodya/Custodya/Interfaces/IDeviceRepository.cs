c89e9699c607588b4edecb002c2cebcf4b153bb4
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-04-29 15:57:15 -0400

    models

---
+++ Mobile_App/Custodya/Custodya/Interfaces/IDeviceRepository.cs
@@ -0,0 +1,16 @@
+﻿using Custodya.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custodya.Interfaces
+{
+    public interface IDeviceRepository<T>
+    {
+        Task<T> GetLatestData();
+        Task<IEnumerable<T>> GetPastData(int points = 10);
+
+    }
+}
