7ef2207e4d0b136e3cdbcc9c429f5d40d20250bc
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-05-06 06:52:55 +0100

    c2d and d2c, codes kinda gross, ill fix it soon. sorry

--- Farm/Farm.py
+++ Farm/Farm.py
@@ -1,26 +1,71 @@
+import asyncio
+import json
+import os
+from dotenv import load_dotenv
 from subsystems.security.SecuritySubSystem import SecuritySubSystem
-from subsystems.plants.PlantsSubSystem import PlantsSubSystem
+from subsystems.plants.plantsSubSystem import PlantsSubSystem
 from subsystems.GeoLocation.GeoLocationSubSystem import GeoLocation 
-
+from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device import MethodResponse
+from azure.iot.device import Message
+from InterFaces.sensors import AReading
+TELEMETRY_TIME = 5
 class Farm:
     def __init__(self) -> None:
         geolocation = GeoLocation()
 
         securitySubSystem = SecuritySubSystem()
         plantsSubSystem = PlantsSubSystem()
-
+        
         self.subsystems = list()
-        #self.subsystems.append(securitySubSystem)
+        self.subsystems.append(securitySubSystem)
         #self.subsystems.append(plantsSubSystem)
-        self.subsystems.append(geolocation)
-    def read_sensors(self):
+        #self.subsystems.append(geolocation)
+    def read_sensors(self) -> list[AReading]:
+        readings = list()
         for subsystem in self.subsystems:
-            print(subsystem.read_sensors())
+            readings.extend(subsystem.read_sensors())
+        return readings;
+
+async def main():
+    load_dotenv()
+    loop = asyncio.get_event_loop()
+    conn_str = os.environ["IOTHUB_DEVICE_CONNECTION_STRING"]
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    farm = Farm()
+    
+    async def telemetryloop():
+        while True:
+            data = readings_to_json(farm.read_sensors())
+            msg = Message(data);
+            await device_client.send_message(msg);
+            await asyncio.sleep(TELEMETRY_TIME);
+    
+    #loop.create_task(telemetryloop())
+    async def method_request_handler(method_request):
+        payload = None
+        if method_request.name == "is_online":
+            payload = {"result":True}
+            status = 200
+        else:
+            payload = {"details":"method name unknown"}
+            status = 400
+        method_response = MethodResponse.create_from_method_request(method_request, status, payload)
+        await device_client.send_method_response(method_response)
+    device_client.on_method_request_received = method_request_handler
+    await telemetryloop()
+
+def readings_to_json(readings:list[AReading]) -> str:
+    values = []
+    for reading in readings:
+        obj = {"type":reading.reading_type, "unit":reading.reading_unit, "value":reading.value}
+        values.append(obj);
+    print(values)
+    return json.dumps(values);
 
 
-def main():
-   farm = Farm()
-   farm.read_sensors()
+
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
