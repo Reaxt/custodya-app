fbc01c9f825b7d640eaf4107d9a1ceb8d0783799
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-05-17 23:19:43 -0400

    INotifyProperty in sensor

--- Mobile_App/Custodya/Custodya/Models/Sensor.cs
+++ Mobile_App/Custodya/Custodya/Models/Sensor.cs
@@ -5,6 +5,7 @@ using LiveChartsCore.SkiaSharpView;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,14 @@ using System.Threading.Tasks;
 namespace Custodya.Models
 {
 
-    public class Sensor
+    public class Sensor : INotifyPropertyChanged
     {
         public static string[] SecuritySensors = new[] { "Motion", "Loudness", "Door" };        
         public static string[] PlantSensors = new[] { "Temperature", "Moisture", "Humidity", "Water" };        
         public static string[] GeoSensors = new[] { "CoordinatesString", "HeadingString", "InTransport"};
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public enum SensorState { Valid, Error }
 
         public string Name { get; set; }
